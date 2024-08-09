5e561e58f5da56af614d25a5c5d79176a811d037
Thibault-be <thibault.dhooghe@gmail.com>
2024-08-09 12:00:27 +0200

    Annotated the classes, but submit is not working.

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -3,14 +3,20 @@ package tacos;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Table
+@EqualsAndHashCode (exclude = "createdAt")
 public class Taco {
 
-    private long id;
+    @Id
+    private Long id;
 
     private Date createdAt = new Date();
 
@@ -20,6 +26,10 @@ public class Taco {
 
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    private List<IngredientRef> ingredients;
+
+    public void addIngredient(Ingredient taco) {
+        this.ingredients.add(new IngredientRef(taco.getId()));
+    }
 
 }
